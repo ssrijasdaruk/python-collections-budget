8748ec79e81f8e3a439da59ba05c39799d33156d
ssrijasdaruk <sirodom.srijasdaruk@accenture.com>
2022-10-24 11:19:46 +1000

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -19,7 +19,17 @@ class BudgetList():
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __iter__(self):
+        iter = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
 
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+                        
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
@@ -28,6 +38,9 @@ def main():
         myBudgetList.append(expense.amount)
 
     print('The count of all expenses: ' + str(len(myBudgetList)))
+
+    for entry in myBudgetList:
+        print(str(entry))
 
 if __name__ == '__main__':
     main()
